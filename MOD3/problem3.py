105db31627d749da385d13e4779d65ae8d615128
Wayne <166666670+wwpr19@users.noreply.github.com>
2025-04-22 22:05:01 -0700

    Create problem3.py

---
+++ MOD3/problem3.py
@@ -0,0 +1,4 @@
+#3. Prompt the user for a sentence and a word to try to find in that sentence. Have the program print out whether the word was found in the sentence. (i.e. True or False)
+text = input("please write a sentence")
+word = input("please write a word")
+match = word in text
