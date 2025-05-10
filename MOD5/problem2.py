84429fd87b8ce0bfd4e0b9b4405a8af31feb3955
Wayne <166666670+wwpr19@users.noreply.github.com>
2025-05-09 21:31:49 -0700

    Update problem2.py

--- MOD5/problem2.py
+++ MOD5/problem2.py
@@ -1,1 +1,9 @@
 
+#2. Define a string variable (e.g., my_string = "Olympic 
+#College"). Use a for loop to print each character on its own 
+#line. Convert each character to uppercase before printing.
+
+Word = "Sonic The hedgehog"
+for char in Word:
+    print(char.upper())
+print("The end of the loop")
