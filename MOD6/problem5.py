2326eb1b12f7d67dda6fcd4e95eb542d5b869545
Wayne <166666670+wwpr19@users.noreply.github.com>
2025-05-15 23:07:18 -0700

    Update problem5.py

--- MOD6/problem5.py
+++ MOD6/problem5.py
@@ -1,1 +1,10 @@
 
+#5. Create a list of integers. Use a loop to build a new
+#list where each element is the square of the corresponding
+#element in the original list. Print the new list.
+
+list = [49,21,45,12,89,4,7]
+newlist = []
+for i in list:
+    newlist.append (i ** 2)
+print(newlist)
