cf3ba7ee13acf3a9025fc2590f48d28dc302cabc
Wayne <166666670+wwpr19@users.noreply.github.com>
2025-05-15 23:01:42 -0700

    Update problem3.py

--- MOD6/problem3.py
+++ MOD6/problem3.py
@@ -1,1 +1,11 @@
 
+#3. Create a list of strings. Write code to create a new
+#list that includes only the strings longer than three
+#characters. Print the resulting filtered list.
+
+S_list = ["fay", "apple", "ten", "potbelly", "sam"]
+newS_list = []
+for i in S_list:
+    if len(i) > 3:
+        newS_list.append(i)
+print(newS_list)
