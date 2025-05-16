868499b84b3a4ba3a4449eb8b17ae25d289ddc73
Wayne <166666670+wwpr19@users.noreply.github.com>
2025-05-15 22:58:58 -0700

    Update problem1.py

--- MOD6/problem1.py
+++ MOD6/problem1.py
@@ -1,1 +1,11 @@
 
+#1. Create a list of integers (you get to pick!).
+#Write code to iterate through the list and calculate the
+#sum of all even numbers. Print the resulting sum.
+
+list = [4,94,24,456,7,8,78,45]
+sum = 0
+for i in list:
+    if i % 2 == 0:
+       sum +=i      
+print(sum)
